6d9c668803a6a3e199713e5202aa2aaa3731f829
Josh Kitzrow <jk94290@uga.edu>
2020-09-15 21:48:29 -0400

    Set up basic screen for game and some initial variables

--- Pong.py
+++ Pong.py
@@ -4,6 +4,16 @@ import pygame, random
 pygame.init()
 clock = pygame.time.Clock()
 
+# Screen variables and configuration
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 960
+game = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Pong")
+
+# Color variables
+bg_color = (0, 0, 0)
+paddle_color = (255, 255, 255)
+
 # Main game loop
 run = True
 while run:
