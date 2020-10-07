458d98c3da8886279e58a0eba88b79a33decb690
Josh Kitzrow <jk94290@uga.edu>
2020-10-07 16:03:07 -0400

    Added more comments and documented functions with Epytext markup

--- Ball.py
+++ Ball.py
@@ -1,6 +1,10 @@
 import pygame, random
 
 class Ball(pygame.Rect):
+    """
+    Represents a ball in the game
+    @param pygame.Rect: Extends the rectangle class in pygame
+    """
 
     # Speed variables
     ball_speed_y = 7
@@ -8,24 +12,61 @@ class Ball(pygame.Rect):
     time_at_score = None
 
     def setSpeedX(self, speedX):
+        """
+        Sets the X speed of the ball
+        @param speedX: The speed which is to be set for X
+        """
+
         self.ball_speed_x = speedX
 
     def setSpeedY(self, speedY):
+        """
+        Sets the Y speed of the ball
+        @param speedY: The speed which is to be set for Y
+        """
         self.ball_speed_y = speedY
     
     def getSpeedX(self):
+        """
+        Gets the X speed of the ball
+        @return: The X speed of the ball
+        """
+
         return self.ball_speed_x
 
     def getSpeedY(self):
+        """
+        Gets the Y speed of the ball
+        @return: The Y speed of the ball
+        """
+
         return self.ball_speed_y
     
     def getTimeScore(self):
+        """
+        Gets the time at which the ball was scored
+        @return: The time in milliseconds when the ball was scored
+        """
+
         return self.time_at_score
     
     def setTimeScore(self, time):
+        """
+        Sets the time when the ball is scored
+        @param time: The time to be set in milliseconds
+        """
+
         self.time_at_score = time
     
     def ballCollision(self, width, height, player, opponent):
+        """
+        Determines if a ball collides with anything
+        @param width: The width of the screen
+        @param height: The height of the screen
+        @param player: The player paddle
+        @param opponent: The opponent paddle
+        """
+
         self.x += self.getSpeedX()
         self.y += self.getSpeedY()
 
@@ -49,6 +90,14 @@ class Ball(pygame.Rect):
             self.setSpeedX(-self.getSpeedX())
     
     def reset(self, width, height, font, game):
+        """
+        Resets the ball to the middle of the game
+        @param width: The width of the game screen
+        @param height: The height of the game screen
+        @param font: The font to use for the text
+        @param game: The game instance
+        """
+
         current_time = pygame.time.get_ticks()
         self.center = ((int)(width / 2), (int)(height / 2))
 
--- Paddle.py
+++ Paddle.py
@@ -1,17 +1,38 @@
 import pygame
 
 class Paddle(pygame.Rect):
+    """
+    Represents a paddle in the game
+    @param pygame.Rect: Extends the rectangle class in pygame
+    """
     
     paddle_speed = 0
     paddle_score = 0
 
     def setSpeed(self, speed):
+        """
+        Sets the paddle speed
+        @param speed: The speed to set
+        """
+
         self.paddle_speed = speed
     
     def getSpeed(self):
+        """
+        Gets the current paddle speed
+        @return: The current paddle speed
+        """
+
         return self.paddle_speed
     
     def paddleCollision(self, height, isAi, ball):
+        """
+        Determines if the paddle collides with anything
+        @param height: The height of the game screen
+        @param isAi: A boolean which tells if the paddle is ai or the player
+        @param ball: The instance of the ball object in the game
+        """
+
         # If paddle is player
         if not isAi:
             self.y += self.getSpeed()
@@ -36,15 +57,35 @@ class Paddle(pygame.Rect):
                 self.bottom = height
 
     def getScore(self):
+        """
+        Gets the current score for the paddle
+        @return: The current score for the paddle
+        """
+
         return self.paddle_score
     
     def scored(self):
+        """
+        Increments the score of the paddle
+        """
+
         self.paddle_score += 1
     
     def resetScore(self):
+        """
+        Resets the paddle's score
+        """
+
         self.paddle_score = 0
     
     def resetPosition(self, width, height, isAi):
+        """
+        Resets the position of the paddle
+        @param width: The width of the game screen
+        @param height: The height of the game screen
+        @param isAi: Boolean which determines if the paddle is ai or the player
+        """
+
         if not isAi:
             self.x = width - 20
             self.y = (int)(height / 2 - 70)
@@ -53,14 +94,28 @@ class Paddle(pygame.Rect):
             self.y = (int)(height / 2 - 70)
 
     def updateScore(self, font, game, width, height, isAi):
+        """
+        Updates the score on the screen
+        @param font: The font to use for the score
+        @param game: An instance of the game
+        @param width: The width of the game screen
+        @param height: The height of the game screen
+        @param isAi: Boolean which determines if the paddle is ai or the player
+        @return: Returns whether the game is over or not
+        """
+
+        # Sets up the score text
         paddle_text = font.render("{}".format(self.getScore()), False, (255, 255, 255))
         end_font = pygame.font.SysFont("Arialbd", 60)
 
+        # If paddle is Ai, display ai score
         if isAi:
             game.blit(paddle_text, ((int)(width / 2 - 40), 20))
+        # If paddle is player, display player score
         else:
             game.blit(paddle_text, ((int)(width / 2 + 30), 20))
 
+        # If player wins, display win screen
         if self.getScore() == 5 and not isAi:
             game_over_text = end_font.render("Game Over. You Win!", False, (0, 0, 0))
             restart_text = end_font.render("Press (R) to restart, or (ESC) to exit.", False, (0, 0, 0))
@@ -69,7 +124,7 @@ class Paddle(pygame.Rect):
             game.blit(restart_text, ((int)(width / 3 - 180), ((int)(height / 2 + 60))))
             
             return True
-
+        # If player loses, display losing screen
         elif self.getScore() == 5 and isAi:
             game_over_text = end_font.render("Game Over. You Lose!", False, (0, 0, 0))
             restart_text = end_font.render("Press (R) to restart, or (ESC) to exit.", False, (0, 0, 0))
